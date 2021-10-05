7678250076438fa9434c0f7811d96800a247ffd8
sukhbirsekhon <sekhonss@mail.uc.edu>
2021-10-05 13:21:05 -0400

    Added java doc for DAO methods

--- src/main/java/com/example/enterprise/dao/IncomeDAO.java
+++ src/main/java/com/example/enterprise/dao/IncomeDAO.java
@@ -11,11 +11,20 @@ public class IncomeDAO implements IIncomeDAO{
 
     List<Income> allIncomeSources = new ArrayList<>();
 
+    /**
+     * Save an income to the allIncomeSources arraylist
+     * @param income an expense given by user
+     * @throws Exception
+     */
     @Override
     public void save(Income income) throws Exception {
         allIncomeSources.add(income);
     }
 
+    /**
+     * List all entered incomes in allIncomeSources arraylist
+     * @return allIncomeSources arraylist
+     */
     @Override
     public List<Income> listAll() {
         return allIncomeSources;

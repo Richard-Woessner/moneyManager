cfc6f83ddc8ac797f051f66e39378fb703693a3a
mkone25 <konemuc@gmail.com>
2021-11-14 22:59:12 -0500

    Update IncomeRepository.java

--- src/main/java/com/example/enterprise/dao/IncomeRepository.java
+++ src/main/java/com/example/enterprise/dao/IncomeRepository.java
@@ -2,10 +2,11 @@ package com.example.enterprise.dao;
 
 import com.example.enterprise.dto.Income;
 import org.springframework.context.annotation.Profile;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.repository.CrudRepository;
 
 @Profile("!test")
-public interface IncomeRepository extends CrudRepository<Income, Integer> {
+public interface IncomeRepository extends CrudRepository<Income, Integer>, JpaSpecificationExecutor<Income> {
 
 
 }
